925a1b91999b07132cd1e032269efbff5beb425d
RohanVSomani <rohanvsomani@gmail.com>
2026-04-15 00:45:47 +0530

    added

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -14,12 +14,16 @@ public class App
     {
         WebDriver driver=new ChromeDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
+        
         driver.manage().window().maximize();
         Thread.sleep(2000);
+        
         driver.findElement(By.id("username")).sendKeys("student");
         Thread.sleep(2000);
+        
         driver.findElement(By.id("password")).sendKeys("Password123");
         Thread.sleep(2000);
+        
         driver.findElement(By.id("submit")).click();
     }
 }
